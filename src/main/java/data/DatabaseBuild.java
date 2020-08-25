3eb73c90463c4e2c9a3f964e803dc497a630c14c
WillLake10 <william.j.lake2000@gmail.com8>
2020-08-25 21:42:45 +0100

    fix appCtx-database-rebuid.xml path ppproblems

--- src/main/java/data/DatabaseBuild.java
+++ src/main/java/data/DatabaseBuild.java
@@ -4,6 +4,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DatabaseBuild {
     public static void main(String[] args) {
-        new ClassPathXmlApplicationContext("src/main/resources/spring/appCtx-database-rebuid.xml");
+        new ClassPathXmlApplicationContext("spring/appCtx-database-rebuid.xml");
     }
 }
